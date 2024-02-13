d05c54cbb0f6075daa5f4f3722bade867b80505e
DanyaKramer <danuagamerproyt@gmail.com>
2024-02-13 22:53:24 +0300

    many changes

---
+++ script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("Left-nav-for-js").style.width = "250px"
+}
+
+function closeNav() {
+    document.getElementById("Left-nav-for-js").style.width = "0"
+}
