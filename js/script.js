192046694bc134b367d642b8e6bcb929526035a2
DanyaKramer <138522909+DanyaKramer@users.noreply.github.com>
2024-02-26 21:24:20 +0300

    Delete script.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-    document.getElementById("Left-nav-for-js").style.width = "250px"
-}
-
-function closeNav() {
-    document.getElementById("Left-nav-for-js").style.width = "0"
-}
